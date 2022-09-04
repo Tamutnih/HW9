6fcbaba2b671d273d28c632a05697fe15b507d5c
tatyana mutnykh <renu07@list.ru>
2022-09-04 15:43:24 +0500

    решение

--- hometask9.cs
+++ hometask9.cs
@@ -26,28 +26,52 @@
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+// Console.Clear();
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int temp = m;
+
+// if (m > n)
+// {
+//     m = n;
+//     n = temp;
+// }
+
+// PrintSumm(m, n, temp = 0);
+
+// void PrintSumm(int m, int n, int summ)
+// {
+//     summ = summ + n;
+//     if (n <= m)
+//     {
+//         Console.Write($"Сумма элементов= {summ} ");
+//         return;
+//     }
+//     PrintSumm(m, n - 1, summ);
+// }
+
+// int InputNumbers(string input)
+// {
+//     Console.Write(input);
+//     int output = Convert.ToInt32(Console.ReadLine());
+//     return output;
+// }
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
 Console.Clear();
 int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
-int temp = m;
 
-if (m > n)
+int functionAkkerman = Ack(m, n);
+
+Console.WriteLine($"A({m}, {n}) = {Ack(m, n)}");
+
+int Ack(int m, int n)
 {
-    m = n;
-    n = temp;
-}
-
-PrintSumm(m, n, temp = 0);
-
-void PrintSumm(int m, int n, int summ)
-{
-    summ = summ + n;
-    if (n <= m)
-    {
-        Console.Write($"Сумма элементов= {summ} ");
-        return;
-    }
-    PrintSumm(m, n - 1, summ);
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input)
@@ -56,3 +80,4 @@ int InputNumbers(string input)
     int output = Convert.ToInt32(Console.ReadLine());
     return output;
 }
+
